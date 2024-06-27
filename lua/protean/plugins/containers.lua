bc3dad8d6e4790a0c8209bd18ce38e11c4f02a6a
jk <joachim.dev@pm.me>
2024-06-27 14:11:13 +0200

    add remote-containers

---
+++ lua/protean/plugins/containers.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "jamestthompson3/nvim-remote-containers",
+    cmd = {
+      "BuildImage",
+      "AttachToContainer",
+      "StartImage",
+      "ComposeUp",
+      "ComposeDown",
+      "ComposeDestroy",
+    },
+  },
+}
