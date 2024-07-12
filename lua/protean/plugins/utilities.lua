d02f1b10532ff7d2df3dd5f3200fcc2acc0e9f5a
jk <joachim.dev@pm.me>
2024-07-12 12:50:18 +0200

    update cloak-config

--- lua/protean/plugins/utilities.lua
+++ lua/protean/plugins/utilities.lua
@@ -10,5 +10,19 @@ return {
         },
       },
     },
+    keys = {
+      {
+        "<leader>tC",
+        "<cmd>CloakToggle<cr>",
+        mode = { "n" },
+        desc = "[T]oggle [C]loaking of secrets",
+      },
+      {
+        "<leader>tP",
+        "<cmd>CloakPreviewLine<cr>",
+        mode = { "n" },
+        desc = "[T]oggle [P]review secret in current line",
+      },
+    },
   },
 }
