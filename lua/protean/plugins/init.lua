ad84fabb5fe1449d052c2235bc5e62e51a288969
jk <joachim.dev@pm.me>
2024-07-22 17:39:14 +0200

    add which-key binding to show buffer local keymaps

--- lua/protean/plugins/init.lua
+++ lua/protean/plugins/init.lua
@@ -115,6 +115,15 @@ return {
   {
     "folke/which-key.nvim",
     opts = {},
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
   },
 
   {
