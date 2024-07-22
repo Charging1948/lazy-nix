1c0d24e0fde765209daafcce3c01c24b1d635bcf
jk <joachim.dev@pm.me>
2024-07-22 17:39:02 +0200

    update default bindings

--- lua/protean/keybinds/default.lua
+++ lua/protean/keybinds/default.lua
@@ -1,9 +1,11 @@
+local wk = require("which-key")
+
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
---
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
@@ -15,5 +17,16 @@ vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+vim.keymap.set("n", "<leader>cf", vim.diagnostic.open_float, { desc = "Open [f]loating diagnostic message" })
+vim.keymap.set("n", "<leader>cd", vim.diagnostic.setloclist, { desc = "Open [d]iagnostics list" })
+
+wk.add({
+  {
+    "<leader>b",
+    group = "buffers",
+    desc = "[B]uffers",
+    expand = function()
+      return require("which-key.extras").expand.buf()
+    end,
+  },
+})
